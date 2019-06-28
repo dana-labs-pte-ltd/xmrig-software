a0a97cacaeb43119c4c9ccdecbb6bc6a8359b5cc
bjzhangwn <bjzhangwn@gmail.com>
2019-06-28 11:31:43 +0800

    upload the source file,changed for simulation

---
+++ build/CMakeFiles/xmrig-asm.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/media/wanner/work/work/software/xmrig/src/crypto/asm/CryptonightR_template.S" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig-asm.dir/src/crypto/asm/CryptonightR_template.S.o"
+  "/media/wanner/work/work/software/xmrig/src/crypto/asm/cn_main_loop.S" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig-asm.dir/src/crypto/asm/cn_main_loop.S.o"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "HAVE_ROTR"
+  "HAVE_SYSLOG_H"
+  "RAPIDJSON_SSE2"
+  "UNICODE"
+  "XMRIG_NO_IPBC"
+  "_GNU_SOURCE"
+  "__STDC_FORMAT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../src/3rdparty/libcpuid"
+  "/usr/include"
+  "../src"
+  "../src/3rdparty"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/xmrig.dir/DependInfo.cmake
@@ -0,0 +1,118 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/media/wanner/work/work/software/xmrig/src/crypto/c_blake256.c" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/crypto/c_blake256.c.o"
+  "/media/wanner/work/work/software/xmrig/src/crypto/c_groestl.c" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/crypto/c_groestl.c.o"
+  "/media/wanner/work/work/software/xmrig/src/crypto/c_jh.c" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/crypto/c_jh.c.o"
+  "/media/wanner/work/work/software/xmrig/src/crypto/c_skein.c" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/crypto/c_skein.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "HAVE_ROTR"
+  "HAVE_SYSLOG_H"
+  "RAPIDJSON_SSE2"
+  "UNICODE"
+  "XMRIG_NO_IPBC"
+  "_GNU_SOURCE"
+  "__STDC_FORMAT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src/3rdparty/libcpuid"
+  "../src"
+  "../src/3rdparty"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/wanner/work/work/software/xmrig/src/App.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/App.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/App_unix.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/App_unix.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/Mem.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/Mem.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/Mem_unix.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/Mem_unix.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/Summary.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/Summary.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/api/Api.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/api/Api.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/api/ApiRouter.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/api/ApiRouter.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/api/NetworkState.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/api/NetworkState.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/base/io/Json.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/base/io/Json.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/base/io/Json_unix.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/base/io/Json_unix.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/base/io/Watcher.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/base/io/Watcher.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/base/kernel/Entry.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/base/kernel/Entry.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/base/kernel/Process.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/base/kernel/Process.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/base/kernel/Signals.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/base/kernel/Signals.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/base/net/Pool.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/base/net/Pool.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/base/net/Pools.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/base/net/Pools.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/base/tools/Arguments.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/base/tools/Arguments.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/base/tools/Handle.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/base/tools/Handle.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/base/tools/String.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/base/tools/String.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/common/Console.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/common/Console.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/common/Platform.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/common/Platform.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/common/Platform_unix.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/common/Platform_unix.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/common/api/HttpRequest.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/common/api/HttpRequest.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/common/api/Httpd.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/common/api/Httpd.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/common/config/CommonConfig.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/common/config/CommonConfig.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/common/config/ConfigLoader.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/common/config/ConfigLoader.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/common/config/ConfigWatcher.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/common/config/ConfigWatcher.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/common/crypto/Algorithm.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/common/crypto/Algorithm.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/common/crypto/keccak.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/common/crypto/keccak.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/common/log/BasicLog.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/common/log/BasicLog.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/common/log/ConsoleLog.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/common/log/ConsoleLog.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/common/log/FileLog.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/common/log/FileLog.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/common/log/Log.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/common/log/Log.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/common/log/SysLog.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/common/log/SysLog.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/common/net/Client.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/common/net/Client.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/common/net/Job.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/common/net/Job.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/common/net/SubmitResult.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/common/net/SubmitResult.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/common/net/Tls.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/common/net/Tls.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/common/net/strategies/FailoverStrategy.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/common/net/strategies/FailoverStrategy.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/common/net/strategies/SinglePoolStrategy.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/common/net/strategies/SinglePoolStrategy.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/core/Config.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/core/Config.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/core/Controller.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/core/Controller.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/core/cpu/AdvancedCpuInfo.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/core/cpu/AdvancedCpuInfo.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/core/cpu/Cpu.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/core/cpu/Cpu.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/crypto/Asm.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/crypto/Asm.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/crypto/CryptonightR_gen.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/crypto/CryptonightR_gen.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/crypto/cn_gpu_avx.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/crypto/cn_gpu_avx.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/crypto/cn_gpu_ssse3.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/crypto/cn_gpu_ssse3.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/net/Network.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/net/Network.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/net/strategies/DonateStrategy.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/net/strategies/DonateStrategy.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/workers/CpuThread.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/workers/CpuThread.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/workers/Handle.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/workers/Handle.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/workers/Hashrate.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/workers/Hashrate.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/workers/MultiWorker.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/workers/MultiWorker.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/workers/Worker.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/workers/Worker.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/workers/Workers.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/workers/Workers.cpp.o"
+  "/media/wanner/work/work/software/xmrig/src/xmrig.cpp" "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig.dir/src/xmrig.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "HAVE_ROTR"
+  "HAVE_SYSLOG_H"
+  "RAPIDJSON_SSE2"
+  "UNICODE"
+  "XMRIG_NO_IPBC"
+  "_GNU_SOURCE"
+  "__STDC_FORMAT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/3rdparty/libcpuid"
+  "../src"
+  "../src/3rdparty"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/media/wanner/work/work/software/xmrig/build/CMakeFiles/xmrig-asm.dir/DependInfo.cmake"
+  "/media/wanner/work/work/software/xmrig/build/src/3rdparty/libcpuid/CMakeFiles/cpuid.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
